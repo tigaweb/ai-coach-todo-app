5f3d04a1f36c05fa4eb329d3aa523c64a67bf166
tigaweb <nka.webtech.dev@gmail.com>
2025-07-23 00:18:45 +0900

    ページナビゲーションの追加

---
+++ frontend/src/navigation/AppNavigator.tsx
@@ -0,0 +1,31 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../types/navigation";
+import React, { useEffect } from "react";
+import { useAuthStore } from "../stores/authStore";
+import { NavigationContainer } from "@react-navigation/native";
+
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const AppNavigator: React.FC = () => {
+  const { isAuthenticated, initialize } = useAuthStore();
+
+  useEffect(() => {
+    initialize();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {isAuthenticated ? (
+          <>test</>
+        ) : (
+          <Stack.Screen name="Login" component={LoginScreen} />
+        )}
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppNavigator;
